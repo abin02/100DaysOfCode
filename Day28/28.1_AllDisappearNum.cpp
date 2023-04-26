cd6b93359f4237dbbe13d3ff9d54f4a578e1c388
abin02 <abinchandran2002@gmail.com>
2023-04-26 20:03:38 +0530

    Day 28

---
+++ Day28/28.1_AllDisappearNum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> findDisappearedNumbers(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> count;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[abs(nums[i]) - 1] > 0)
+            {
+                nums[abs(nums[i]) - 1] *= -1;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] > 0)
+            {
+                count.push_back(i + 1);
+            }
+        }
+        return count;
+    }
+};
