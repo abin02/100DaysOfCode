3cfcdcea04955e5d01a68e65e41c45eae17afe5c
abin02 <abinchandran2002@gmail.com>
2023-04-30 20:16:01 +0530

    Day 31 q2

---
+++ Day31/31.2_addBinaryStrings.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+string addBinary(string A, string B)
+{
+    int s, m = A.size() - 1, n = B.size() - 1, c = 0;
+    string ans;
+    while (m >= 0 && n >= 0)
+    {
+        // cout<< (int(A[m])-'0')<<":"<<(int(B[n])-'0')<<endl;
+        if ((int(A[m]) - '0') + (int(B[n]) - '0') + c == 1)
+        {
+            c = 0;
+            ans = '1' + ans;
+        }
+        else if ((int(A[m]) - '0') + (int(B[n]) - '0') + c == 2)
+        {
+            ans = '0' + ans;
+            c = 1;
+        }
+        else if ((int(A[m]) - '0') + (int(B[n]) - '0') + c == 0)
+        {
+            ans = '0' + ans;
+        }
+        else
+        {
+            c = 1;
+            ans = '1' + ans;
+        }
+        m--;
+        n--;
+    }
+    while (m >= 0)
+    {
+        if (c + (int(A[m]) - '0') > 1)
+        {
+            ans = '0' + ans;
+        }
+        else if (c + (int(A[m]) - '0') == 1)
+        {
+            ans = '1' + ans;
+            c = 0;
+        }
+        else
+        {
+            ans = '0' + ans;
+        }
+        m--;
+    }
+    while (n >= 0)
+    {
+        // cout<<c<<endl
+        if (c + (int(B[n]) - '0') > 1)
+        {
+            ans = '0' + ans;
+        }
+        else if (c + (int(B[n]) - '0') == 1)
+        {
+            ans = '1' + ans;
+            c = 0;
+        }
+        else
+        {
+            ans = '0' + ans;
+        }
+        n--;
+    }
+    if (c > 0)
+    {
+        ans = '1' + ans;
+    }
+    return ans;
+}
