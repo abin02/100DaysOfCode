df9f7cef0c2340bf42ff9d9d0a19de25e234927f
Abin Chandran <109299768+abin02@users.noreply.github.com>
2023-06-03 20:02:47 +0530

    Update test. Try new method.

--- Test/39.splitFunction.cpp
+++ Test/39.splitFunction.cpp
@@ -3,12 +3,18 @@ using namespace std;
 void tokenize(string s, string del = " ")
 {
     int start, end = -1 * del.size();
-    do
-    {
-        start = end + del.size();
-        end = s.find(del, start);
-        cout << s.substr(start, end - start) << endl;
-    } while (end != -1);
+    //do
+    //{
+    //    start = end + del.size();
+    //    end = s.find(del, start);
+    //    cout << s.substr(start, end - start) << endl;
+    //} while (end != -1);
+    stringstream str1(s);
+    string str2;
+    while(!str1){
+        str1>>str2;
+        cout<<str2;
+    }
 }
 int main()
 {
