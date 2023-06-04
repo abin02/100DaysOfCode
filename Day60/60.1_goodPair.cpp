f8c251f611482c865b83881d25317cd4a1ef78cd
abin02 <abinchandran2002@gmail.com>
2023-06-04 17:54:52 +0530

    Day 60 q1

---
+++ Day60/60.1_goodPair.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int numIdenticalPairs(vector<int> &nums)
+    {
+        int n = nums.size(), count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
