4b2e74cd8a9cd906b10e311e6904385e7d7d13cb
abin02 <abinchandran2002@gmail.com>
2023-06-07 10:13:33 +0530

    Day 62 q1

---
+++ Day62/62.1_RunningSum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> runningSum(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> ans(n);
+        ans[0] = nums[0];
+        for (int i = 1; i < n; i++)
+        {
+            ans[i] = ans[i - 1] + nums[i];
+        }
+        return ans;
+    }
+};
