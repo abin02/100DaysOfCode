54bb28072e460f118308e55b2769abd20df86c27
Abin Chandran <109299768+abin02@users.noreply.github.com>
2023-08-08 18:47:51 +0530

    Update 97.9_2Dsearch.cpp

--- Day97/97.9_2Dsearch.cpp
+++ Day97/97.9_2Dsearch.cpp
@@ -36,4 +36,19 @@ public:
         }
         return false;
     }
+    //alternative way
+    int i=0,j=matrix[0].size()-1,n=matrix.size();
+        while(i<n && j>=0){
+            if(matrix[i][j]==target){
+                return true;
+            }
+            if(matrix[i][j] > target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return false;
+    }
 };
